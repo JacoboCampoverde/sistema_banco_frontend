9d6cb5a7d1375e9960bdc57596ef54cdfec578c0
JacoboCampoverde <153460068+JacoboCampoverde@users.noreply.github.com>
2025-07-29 22:00:33 -0500

    Update link.ts

--- src/app/shared/link.ts
+++ src/app/shared/link.ts
@@ -1,1 +1,2 @@
-export const direccionApi = 'http://localhost:3000';
+//export const direccionApi = 'http://localhost:3000';
+export const direccionApi = 'https://sistema-banco-backend.onrender.com';
